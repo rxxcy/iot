ee278e6c7312b4da5d8aecbf3bb22662695a2292
rxxcy <rxxcy@vip.qq.com>
2022-04-13 23:04:23 +0800

    修改计时错误

--- src/middleware/app.middleware.ts
+++ src/middleware/app.middleware.ts
@@ -2,5 +2,5 @@ export const logger = async (request, res, next) => {
   const start = Date.now();
   const { method, url } = request;
   await next();
-  console.log(`[ ${method} ] Gwei ${start - Date.now()} ms -> ${url}`);
+  console.log(`[ ${method} ] Gwei ${Date.now() - start} ms -> ${url}`);
 };
